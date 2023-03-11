6473a1247c826de521c3def1ba5a2617545cd433
corleone113 <corleonexiao@qq.com>
2023-03-11 22:44:10 +0800

    删除无关的代码

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-// import Matting from './components/Matting.vue'
-import Some from './components/ParentSome.vue'
+import Matting from './components/Matting.vue'
 </script>
 
 <template>
 	<!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-	<!-- <Matting /> -->
-	<Some />
+		<Matting />
 </template>
 
 <style>
 body {
 	margin: 0;
 }
+
 #app {
 	font-family: Avenir, Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
